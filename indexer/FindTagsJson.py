b90e1692998f5a236cc175bbad8429fcf3f07906
mila <milag@kth.se>
2019-05-15 17:01:16 +0200

    9500 tagged articles

--- indexer/FindTagsJson.py
+++ indexer/FindTagsJson.py
@@ -7,9 +7,11 @@ import sys
 from nltk.corpus import stopwords
 from collections import Counter
 import string
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 stop = stopwords.words('english')
-stop.extend(["''", "´´", "``", "’"])
+stop.extend(["''", "´´", "``", "’", "‘"])
 punct = string.punctuation
 global length
 title_matrix = list()
@@ -34,63 +36,58 @@ def handle_content(content):
                 and not token in punct and not has_digit(token) \
                 and not "'" in token]
      cnt = Counter(filtered)
-     return [word for word,nr in cnt.most_common(10)]
+     return [word for word,nr in cnt.most_common(20)]
  
 def find_bad_words():
     bad_words = list()
-    for word,nr in all_content_words.items():
-        if nr > length*0.1:
+    for word,nr in all_content_words.iteritems():
+        if nr > length*0.001:
             bad_words.append(word)
     return bad_words
         
-def remove_bad_words(bad_words):
-    for row in content_matrix:
-        for word in row:
-            if word in bad_words:
-                row.remove(word)
-        
-def concatenate_tags():
+def concatenate_tags(bad_words):
     for i in range(length):
         t = title_matrix[i]
         c = content_matrix[i]
-        tags_matrix.append(set(t + c))
+        c_updated = list()
+        for word in c:
+            if not word in bad_words:
+                c_updated.append(word)
+        tags_matrix.append(set(c_updated + t))
             
 def main():    
     directory = "../crawled_data"
     global length
     length = len(os.listdir(directory))
     for filename in os.listdir(directory):
-            with open("./" + directory + "/" + filename, encoding = 'utf8') as file:  
-                try : 
+            with open("./" + directory + "/" + filename) as file:  
+                try:
                     data = json.load(file)
-                except :
-                    length = length - 1
-                    continue
-                title = data['title']
-                title_matrix.append(handle_title(title))
-                content = data['content']
-                content_words = handle_content(content)
-                content_matrix.append(content_words)
-                all_content_words.update(content_words)
-            file.close
-                
+                    title = data['title']
+                    title_matrix.append(handle_title(title))
+                    content = data['content']
+                    content_words = handle_content(content)
+                    content_matrix.append(content_words)
+                    all_content_words.update(content_words)
+                except:
+                    print(filename)
+                file.close
+    
     bad_words = find_bad_words()
-    remove_bad_words(bad_words)
-    concatenate_tags()
+    concatenate_tags(bad_words)
 
     i = 0
     for filename in os.listdir(directory):
-        with open("./" + directory + "/" + filename, encoding = 'utf8') as file:  
-            try :
-               data = json.load(file)
-            except :
-                continue
+        with open("./" + directory + "/" + filename) as file:  
+            data = json.load(file)
             data['tags'] = ' '.join(str(s) for s in list(tags_matrix[i]))
             i += 1
         file.close
-        with open("./" + directory + "/" + filename, "w", encoding = 'utf8') as outfile:  
+        with open("./" + directory + "/" + filename, "w") as outfile:  
             json.dump(data, outfile)
         outfile.close
     
 if __name__== "__main__":
   main()
+
+    
